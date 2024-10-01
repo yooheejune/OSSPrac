e8092597809290128166c922e98944da9f24a0ef
yooheejun <gmlwns7497@dgu.ac.kr>
2024-10-01 16:19:34 +0900

    add grade

--- Prac5/main.py
+++ Prac5/main.py
@@ -1,9 +1,12 @@
 name = input("이름을 입력하세요 : ")
 number = input("학번을 입력하세요 : ")
 major = input('학과를 입력하세요 : ')
+grade = input('학년을 입력하세요: ')
 
 print("<출력>")
 print(f"이름: {name}")
 print(f"학번: {number}")
 print(f'학과: {major}')
+print(f'학년: {grade}' ) 
+
 
